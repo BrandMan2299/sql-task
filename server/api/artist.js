e7804a28335057c1414f11599a2975eaaa6cf9a4
itai brand <67857476+BrandMan2299@users.noreply.github.com>
2020-09-24 21:29:50 +0300

    song api

--- server/api/artist.js
+++ server/api/artist.js
@@ -14,22 +14,20 @@ router.post('/', async (req, res) => {
     const newArtist = await Artist.create(req.body);
     res.json(newArtist)
 })
-
-router.get('/:artistId', async (req, res) => {
-    const artist = await Artist.findByPk(req.params.artistId);
-    res.json(artist)
-})
-
-router.patch('/:artistId', async (req, res) => {
-    const artist = await Artist.findByPk(req.params.artistId);
-    await artist.update(req.body);
-    res.json(artist)
-})
-
-router.delete('/:artistId', async (req, res) => {
-    const artist = await Artist.findByPk(req.params.artistId);
-    await artist.destroy();
-    res.json({ deleted: true })
-})
+router.route('/:artistId')
+    .get(async (req, res) => {
+        const artist = await Artist.findByPk(req.params.artistId);
+        res.json(artist)
+    })
+    .put(async (req, res) => {
+        const artist = await Artist.findByPk(req.params.artistId);
+        await artist.update(req.body);
+        res.json(artist)
+    })
+    .delete(async (req, res) => {
+        const artist = await Artist.findByPk(req.params.artistId);
+        await artist.destroy();
+        res.json({ deleted: true })
+    })
 
 module.exports = router;
--- server/api/song.js
+++ server/api/song.js
@@ -1,21 +1,35 @@
 const { Router } = require('express');
-const { Song, Playlist } = require('../models');
-
+const { Song, Playlist, Album, Artist } = require('../models');
 
 const router = Router();
+router.route('/')
+    .get(async (req, res) => {
+        const allSongs = await Song.findAll({
+            include: [{ model: Album, attributes: ['name'] }, { model: Artist, attributes: ['name'] }, { model: Playlist, attributes: ['name'] }]
+        });
+        res.json(allSongs)
+    })
+    .post(async (req, res) => {
+        const song = await Song.create(req.body);
+        res.json(song)
+    })
 
-router.get('/', async (req, res) => {
-    const allSongs = await Song.findAll({
-        include: ['Album', 'Artist', { model: Playlist, attributes: ['name'] }]
-    });
-    res.json(allSongs)
-})
-
-router.get('/:songId', async (req, res) => {
-    const allSongs = await Song.findByPk(req.params.songId, {
-        include: ['Album', 'Artist']
-    });
-    res.json(allSongs)
-})
+router.route('/:songId')
+    .get(async (req, res) => {
+        const song = await Song.findByPk(req.params.songId, {
+            include: [{ model: Album, attributes: ['name'] }, { model: Artist, attributes: ['name'] }, { model: Playlist, attributes: ['name'] }]
+        });
+        res.json(song)
+    })
+    .put(async (req, res) => {
+        const song = await Song.findByPk(req.params.songId);
+        await song.update(req.body);
+        res.json(song)
+    })
+    .delete(async (req, res) => {
+        const song = await Song.findByPk(req.params.songId);
+        await song.destroy();
+        res.json({ deleted: true })
+    })
 
 module.exports = router;
